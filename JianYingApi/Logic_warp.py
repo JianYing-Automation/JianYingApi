17e2cbacfc4a5b6a243fd5031d1f4d99fe2b320a
PPPPP <ppzzhh1186861238@hotmail.com>
2022-09-10 10:26:50 +0800

    Try to fix #12

--- JianYingApi/Logic_warp.py
+++ JianYingApi/Logic_warp.py
@@ -14,11 +14,12 @@ def lag(lag:float=lag_t): time.sleep(lag)
 
 def _creat_exe(exepath:str): return multiprocessing.Process(target=os.system,args=(exepath,))
 
-def _kill_jianYing(): 
-    def _has_running()->bool:
+def _has_running()->bool:
         if ("jianyingpro") in os.popen("tasklist").read().lower():return True
         if ("vedetector") in os.popen("tasklist").read().lower():return True
         return False
+
+def _kill_jianYing(): 
     while _has_running():
         subprocess.Popen('%s%s' % ("taskkill /F /T /IM ","VEDetector.exe"),stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL).wait()
         subprocess.Popen('%s%s' % ("taskkill /F /T /IM ","JianYingPro.exe"),stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL).wait()
@@ -31,7 +32,7 @@ def _Get_JianYing_Default_Path()->str:
 
 def _install_JianYing(Installer_Path:str):
     _path_will_install = os.path.join(_Get_JianYing_Default_Path() , "Apps" , "Configure.ini")
-    assert os.path.exists(_path_will_install) == False , "Has Been Installed!"
+    assert os.path.exists(os.path.join(_path_will_install,"Apps","JianyingPro.exe")) == False , "Has Been Installed!"
     __install_process = _creat_exe(Installer_Path)
     __install_process.start()
     while not api32.WindowControl(searchDepth=1,ClassName="#32770").Exists() : lag() # Keep it Roll until it turns up
@@ -42,3 +43,4 @@ def _install_JianYing(Installer_Path:str):
     while not os.path.exists(_path_will_install): lag()
     __install_process.terminate()
     _kill_jianYing()
+    print("Install Finished")
