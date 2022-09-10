7dca8412a32b8fc5a6fe366e1bcf344bbff0cde4
PPPPP <ppzzhh1186861238@hotmail.com>
2022-09-10 15:35:25 +0800

    fix bugs

--- JianYingApi/Logic_warp.py
+++ JianYingApi/Logic_warp.py
@@ -5,6 +5,7 @@
 import os
 import subprocess
 import multiprocessing
+import subprocess
 import uiautomation as api32
 import time
 
@@ -35,14 +36,14 @@ def echo(message):
 def _install_JianYing(Installer_Path:str):
     _path_will_install = os.path.join(_Get_JianYing_Default_Path() , "Apps" , "Configure.ini")
     assert os.path.exists(os.path.join(_path_will_install,"Apps","JianyingPro.exe")) == False , "Has Been Installed!"
-    __install_process = _creat_exe(Installer_Path)
-    __install_process.start()
+    _install_process = _creat_exe(Installer_Path)
+    _install_process.start()
     while not api32.WindowControl(searchDepth=1,ClassName="#32770").Exists() : lag() # Keep it Roll until it turns up
-    __install_inst = api32.WindowControl(searchDepth=1,ClassName="#32770")
+    _install_inst = api32.WindowControl(searchDepth=1,ClassName="#32770")
     api32.Click(
-        x=__install_inst.BoundingRectangle.xcenter(),
-        y=int(__install_inst.BoundingRectangle.ycenter()-__install_inst.BoundingRectangle.height()/8))
+        x=_install_inst.BoundingRectangle.xcenter(),
+        y=int(_install_inst.BoundingRectangle.ycenter()-_install_inst.BoundingRectangle.height()/8))
     while not os.path.exists(_path_will_install): lag()
-    __install_process.terminate()
+    _install_process.terminate()
     _kill_jianYing()
     echo("Install Finished")
