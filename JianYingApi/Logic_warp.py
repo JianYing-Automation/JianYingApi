e5310803cc9661c30a2bf6e7071a3369ad2ac942
PPPPP <ppzzhh1186861238@hotmail.com>
2022-09-10 16:29:44 +0800

    Optimize Vedetector Logic

--- JianYingApi/Logic_warp.py
+++ JianYingApi/Logic_warp.py
@@ -2,26 +2,24 @@
     Some Logic Warps
 
 """
+from genericpath import isdir
 import os
 import subprocess
 import multiprocessing
-import subprocess
 import uiautomation as api32
 import time
 
 lag_t = 0.5
 def lag(lag:float=lag_t): time.sleep(lag)
 
-def _creat_exe(exepath:str): return multiprocessing.Process(target=os.system,args=(exepath,),daemon=True)
+def _creat_exe(exepath:str): return multiprocessing.Process(target=os.system,args=(exepath,),daemon=True).is_alive
 
 def _has_running()->bool:
         if ("jianyingpro") in os.popen("tasklist").read().lower():return True
-        if ("vedetector") in os.popen("tasklist").read().lower():return True
         return False
 
 def _kill_jianYing(): 
     while _has_running():
-        subprocess.Popen('%s%s' % ("taskkill /F /T /IM ","VEDetector.exe"),stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL).wait()
         subprocess.Popen('%s%s' % ("taskkill /F /T /IM ","JianYingPro.exe"),stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL).wait()
         subprocess.Popen('%s%s' % ("taskkill /F /T /IM ","jy.exe"),stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL).wait()
         time.sleep(0.5)
@@ -44,6 +42,9 @@ def _install_JianYing(Installer_Path:str):
         x=_install_inst.BoundingRectangle.xcenter(),
         y=int(_install_inst.BoundingRectangle.ycenter()-_install_inst.BoundingRectangle.height()/8))
     while not os.path.exists(_path_will_install): lag()
-    _install_process.terminate()
-    _kill_jianYing()
+    for i in os.listdir(os.path.join(_Get_JianYing_Default_Path(),"Apps")):
+        if os.path.isdir(os.path.join(_Get_JianYing_Default_Path(),"Apps",i)):
+            if "VEDetector.exe" in os.listdir(os.path.join(_Get_JianYing_Default_Path(),"Apps",i)):
+                os.remove(os.path.join(_Get_JianYing_Default_Path(),"Apps",i,"VEDetector.exe"))
+                echo("Removed Ved")
     echo("Install Finished")
